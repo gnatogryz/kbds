a153d0210b1324c499192ea020f5dfb115b01047
Michał Dobrzański <plague@go2.pl>
2022-05-15 17:22:41 +0200

    fix: 1000hz + chording

---
+++ dobrzan/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define USB_POLLING_INTERVAL_MS 1
+#define QMK_KEYS_PER_SCAN 2
+#undef  PRODUCT
+#define PRODUCT         Gnatboard
+#undef  MANUFACTURER
+#define MANUFACTURER    Dobrzan
