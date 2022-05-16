c3f4759d97f31a86cca59bccd1b570d411fb77d3
Michał Dobrzański <plague@go2.pl>
2022-05-16 10:52:28 +0200

    fix: final3

--- qmk_firmware/keyboards/dz60/keymaps/dobrzan/config.h
+++ qmk_firmware/keyboards/dz60/keymaps/dobrzan/config.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #define USB_POLLING_INTERVAL_MS 1
-#define QMK_KEYS_PER_SCAN 4
+
+#define QMK_KEYS_PER_SCAN 8
+
 #undef  PRODUCT
 #define PRODUCT         GNAT60 Najlepsza Klawiatura
 #undef  MANUFACTURER
 #define MANUFACTURER    Dobrzan
+
+#define FORCE_NKRO
 
 #define BOOTMAGIC_LITE_ROW 0
 #define BOOTMAGIC_LITE_COLUMN 0
